57f19f502a6e93d0998449645b727070c80678ba
HaoHT21 <haotran212006@gmail.com>
2025-05-21 17:29:06 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //a
+            var x = 10;
+            Console.WriteLine(x);
+
+            //b
+            getDetail(10);
+            getDetail("Games");
+            getDetail(true);
+            getDetail(9.5);
+
+            Console.ReadLine();
+
+
+
+            //c
+            
+        
+            userData data = new userData("2024", "Hao", 100);
+            data.GetInfor();
+
+
+            
+        
+
+
+        }
+        static void getDetail(dynamic value)
+        {
+            Console.WriteLine(value);
+        }
+        
+
+
+
+
+
+    }
+}
---
+++ userData.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab02
+{
+    public class userData
+    {
+        public string Id;
+        public string Username;
+        public int Level;
+        
+
+        public userData(string id,string username, int level)
+        {
+            Id = id;
+            Username = username;
+            Level = level;
+        }
+        public void GetInfor()
+        {
+            Console.WriteLine(":ID: " + Id + "|userName: " + Username + "|level: " + Level);
+
+        }
+
+
+        
+
+
+
+    }
+    
+}
